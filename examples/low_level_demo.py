7f0f4e78c5b91bf2922d20fa108246596d826815
mertunsaldi <unsaldimert@gmail.com>
2025-06-22 01:45:00 +0300

    Wrap low level demo in main

--- examples/low_level_demo.py
+++ examples/low_level_demo.py
@@ -7,21 +7,28 @@ send direct commands to a PlotClock.
 import time
 from ball_example.game_api import GameAPI
 
-api = GameAPI()
-api.start()
 
-while True:
-    time.sleep(0.1)
-    with api.lock:
-        balls = list(api.balls)
-        markers = list(api.arucos)
-    print("balls", [b.center for b in balls])
-    print("markers", [m.id for m in markers])
+def main() -> None:
+    """Run the low level demo."""
+    api = GameAPI()
+    api.start()
 
-    if balls and not api.pico_connected:
-        try:
-            api.connect_pico()
-            api.send_cmd("home")
-        except Exception as e:
-            print("pico error", e)
+    while True:
+        time.sleep(0.1)
+        with api.lock:
+            balls = list(api.balls)
+            markers = list(api.arucos)
+        print("balls", [b.center for b in balls])
+        print("markers", [m.id for m in markers])
+
+        if balls and not api.pico_connected:
+            try:
+                api.connect_pico()
+                api.send_cmd("home")
+            except Exception as e:
+                print("pico error", e)
+
+
+if __name__ == "__main__":
+    main()
 
