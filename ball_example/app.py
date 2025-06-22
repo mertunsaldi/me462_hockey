5b53b079ea79a214a17e9015e4dae926eb0dc195
mertunsaldi <unsaldimert@gmail.com>
2025-06-22 11:57:59 +0300

    cam source set

--- ball_example/app.py
+++ ball_example/app.py
@@ -20,6 +20,7 @@ app = Flask(__name__, template_folder="templates", static_folder="static")
 
 # Core processing API
 api = GameAPI()
+api.set_cam_source(1)
 
 if hasattr(app, "before_first_request"):
     @app.before_first_request
